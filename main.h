165f68e0ad8ded8379465a924a6004325d651de2
Mirjamol Mirislomov <96412090+holmon-alp@users.noreply.github.com>
2022-08-31 22:45:36 +0500

    new error messages

--- main.h
+++ main.h
@@ -11,7 +11,11 @@
 #define ENDL  putchar('\n')
 #define MAX_READ_SIZE 200
 
-#define error_node_exist(n) (printf("node doesn't exist (%d)\n", n))
+#define error_node_exist(n)         (printf("node doesn't exist (%d)\n", n))
+#define error_node_alrdy_exist(n)   (printf("this node already exists (%d)\n", n))
+#define error_block_alrdy_exist(n)  (printf("this block already exists (%s)\n", n))
+#define error_block_exist(n)        (printf("block doesn't exist (%s)\n", n))
+#define error_commd                 printf("command not found\n")
 
 
 typedef struct s_bocklist {
@@ -25,3 +29,4 @@ typedef struct s_nodelist {
     blocklist* blocklist;
     struct s_nodelist* next;
 } nodelist;
+
